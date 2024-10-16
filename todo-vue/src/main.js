cf53150f817e3c9553c6875772ec9df6a75e66de
Leandro Campos <leandro.f.ti@outlook.com>
2024-10-15 21:19:51 -0300

    ajustando 03

--- todo-vue/src/main.js
+++ todo-vue/src/main.js
@@ -1,6 +1,6 @@
-import { createApp } from 'vue'
-import App from './index.html'
-
+import { createApp } from 'vue';
+import App from './App.vue'; // Aqui deve ser o App.vue
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
+
